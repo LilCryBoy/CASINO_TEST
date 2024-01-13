95755a80792c1446aed505f332a332f075e9d1bf
Тёмыч <45329541+LilCryBoy@users.noreply.github.com>
2024-01-13 21:58:48 +0500

    ADD TIMER CLASS + THREAD

---
+++ CASINO/Timer.cpp
@@ -0,0 +1,14 @@
+﻿#include "Timer.h"
+#include <thread>
+
+TIMER::TIMER(){
+}
+
+void TIMER::Start(std::chrono::microseconds delay, std::function<void()> callback)
+{
+    std::thread([=]()
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(delay));
+        callback;
+    }).detach();
+}
---
+++ CASINO/Timer.h
@@ -0,0 +1,13 @@
+﻿
+
+//Простой класс таймера
+
+#include <chrono>
+#include <functional>
+
+class TIMER
+{
+public:
+    TIMER();
+    void Start(std::chrono::microseconds delay, std::function<void ()> callback);
+};
